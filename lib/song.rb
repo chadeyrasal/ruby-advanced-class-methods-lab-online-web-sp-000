fb7b63d944723e68d1e2bc0bef7112fb1e5af2e4
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-12-05 17:55:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,7 @@
 class Song
+
   attr_accessor :name, :artist_name
+
   @@all = []
 
   def self.all
@@ -8,6 +10,39 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    @name = name
+    song = self.new
+  end
+
+  def self.create_by_name(name)
+  end
+
+  def self.find_by_name(name)
+  end
+
+  def self.find_or_create_by_name(name)
+  end
+
+  def self.alphabetical(name)
+  end
+
+  def self.new_from_filename(filename)
+  end
+
+  def self.create_from_filename(filename)
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
